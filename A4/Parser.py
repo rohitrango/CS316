c356d39232239a0d5bf101b76925140dc167a9f5
rohitrango <rohit.rango@gmail.com>
2018-03-14 16:14:34 +0530

    A4 files added

--- A4/Parser.py
+++ A4/Parser.py
@@ -20,9 +20,10 @@ reserved = {
 	'if' 	: 'IF',
 	'else'	: 'ELSE',
 	'while' : 'WHILE',
-	'main'  : 'MAIN',
+
 	'void' 	: 'VOID',
 	'int'	: 'INT',
+	'float'	: 'FLOAT',
 }
 
 tokens += list(reserved.values())
@@ -104,16 +105,105 @@ precedence = (
 
 # This defines our program
 def p_def_prog(p):
-	''' prog : VOID MAIN LPAREN RPAREN LCURL body RCURL
-			 | VOID MAIN LPAREN RPAREN LCURL RCURL
+	''' prog : declarations procedures
 	'''
-	if len(p) == 7:
-		p[0] = AbstractSyntaxTreeNode("PROG", [AbstractBodyTreeNode("BODY", [])])
+	p[0] = AbstractSyntaxTreeNode("PROG", [p[1], p[2]])
+	cfg_ast.append(p[0])
+
+# This takes care of any number of declarations first
+def p_def_declaration(p):
+	''' 
+		declarations : declarations decl SEMICOLON
+				     | 
+	'''
+	if len(p) == 1:
+		p[0] = AbstractSyntaxTreeNode("DECLARATIONS", [])
 	else:
-		p[0] = AbstractSyntaxTreeNode("PROG", [p[6]])
-	cfg_ast.append(p[6])
+		p[0] = p[1]
+		p[0].addChild(p[2])
+
+# Contain one or more procedures
+def p_def_procedures(p):
+	'''
+		procedures : procedures procedure
+				  | procedure 	
+	'''
+	if len(p) == 2:
+		p[0] = AbstractSyntaxTreeNode("PROCEDURES", [p[1]])
+	else:
+		p[0] = p[2]
+		p[0].addChild(p[1])
+
+def p_def_type(p):
+	'''
+		type : VOID 
+			 | INT
+			 | FLOAT
+	'''
+	if isinstance(p[1], str):
+		p[1] = AbstractSyntaxTreeNode("TYPE", [], [p[1]])
+	p[0] = p[1]
+
+# def p_def_type_nonvoid(p):
+# 	'''
+# 		type_nonvoid : 	  INT
+# 						| FLOAT
+# 	'''
+# 	p[0] = AbstractSyntaxTreeNode("TYPE", [], p[0])
 
 
+
+
+# Single procedure
+def p_def_procedure(p):
+	'''
+		procedure : type ID LPAREN params RPAREN LCURL declarations body RCURL
+	'''
+	# Give it the name as that of the ID
+	# It's children are params, declarations, statements
+	p[0] = AbstractSyntaxTreeNode("PROCEDURE", [p[4], p[7], p[8]], p[2])
+
+
+# Define parameters
+def p_def_parameters(p):
+	'''
+		params 	: type ID paramslist 
+				| type ptr paramslist
+				| type addr paramslist
+			   	| 
+	'''
+	if len(p) == 1:
+		p[0] = AbstractSyntaxTreeNode("PARAMS", [])
+	else:
+		# Check for identifiers
+		if isinstance(p[2], str):
+			p[2] = AbstractSyntaxTreeNode("VAR", [], p[2])
+	
+		# p[3] gives a params list, simply add the new parameter to the list in the beginning
+		p[0] = p[3]
+		p[0].operands.insert(0, p[2])
+
+# def paramslist
+def p_def_paramslist(p):
+	'''
+		paramslist 	: COMMA type ID paramslist
+					| COMMA type addr paramslist
+					| COMMA type ptr paramslist
+					| 
+	'''
+	if len(p) == 1:
+		p[0] = AbstractSyntaxTreeNode("PARAMS", [])
+	else:
+		if isinstance(p[3], str):
+			p[3] = AbstractSyntaxTreeNode("VAR", [], p[3])
+		
+		# p[4] gives a params list, 
+		# simply add the new parameter to the list in the beginning
+		p[0] = p[4]
+		p[0].operands.insert(0, p[3])
+
+
+# define body (doesn't contain assignments)
 def p_def_body(p):
 	''' body : body stmt
 			 | stmt
@@ -126,12 +216,47 @@ def p_def_body(p):
 
 
 def p_def_stmt(p):
-	''' stmt : decl SEMICOLON 
-			 | assgn SEMICOLON
+	''' stmt : assgn SEMICOLON
 			 | if_stmt 
 			 | while_stmt
+			 | function_call
 	'''
 	p[0] = p[1]
+
+
+def p_def_function_call(p):
+	''' function_call : ID LPAREN opt_params RPAREN SEMICOLON
+	'''
+	p[0] = AbstractSyntaxTreeNode("FN_CALL", [p[3]] , p[1])
+
+# Optional params to function list
+def p_def_opt_params(p):
+	''' opt_params  : ID opt_params_list
+					| ptr opt_params_list
+					| addr opt_params_list
+					| 
+
+		opt_params_list : COMMA ID opt_params_list
+						| COMMA ptr opt_params_list
+						| COMMA addr opt_params_list
+						|
+	'''
+	if len(p) == 1:
+		# Base case
+		p[0] = AbstractSyntaxTreeNode("OPT_PARAMS", [])
+	elif len(p) == 3:
+		# opt_params recursion
+		if isinstance(p[1], str):
+			p[1] = AbstractSyntaxTreeNode("VAR", [], p[1])
+		p[0] = p[2]
+		p[0].operands.insert(0, p[1])
+	else:
+		# This is for opt_params_list
+		if isinstance(p[2], str):
+			p[2] = AbstractSyntaxTreeNode("VAR", [], p[2])
+		p[0] = p[3]
+		p[0].operands.insert(0, p[2])
+
 
 
 # Adding for A3 -> if and while
@@ -185,7 +310,7 @@ def p_def_assgn(p):
 
 
 def p_def_decl(p):
-	''' decl : INT decl_list
+	''' decl : type decl_list
 	'''
 	p[0] = AbstractSyntaxTreeNode("DECL", [p[2]])
 
@@ -383,24 +508,27 @@ if __name__ == "__main__":
 	with open(filename, 'r') as f:
 		data = f.read()
 
-	ast_file = open(filename + '.ast', 'w')
-	cfg_file = open(filename + '.cfg', 'w')
+	# ast_file = open(filename + '.ast', 'w')
+	# cfg_file = open(filename + '.cfg', 'w')
 
 	lex.input(data)
 	yacc.parse(data)
 
+	for c in cfg_ast:
+		print(c)
+
 	# Here, we check for errors first. If there are no errors, then we are good to go
 	# Print the CFG and ast on 2 different files now
-	error_assignments, messages = check_error_in_assignments(ast_list)
-	if not error_assignments:
-		cfg = generateCFG(cfg_ast[0])
-		# Write the AST file
-		ast_file.write(repr(cfg_ast[0]))
-		# Write the CFG file
-		cfg_file.write("\n".join([repr(x) for x in cfg]))
-	else:
-		for message in messages:
-			print(message)
+	# error_assignments, messages = check_error_in_assignments(ast_list)
+	# if not error_assignments:
+	# 	cfg = generateCFG(cfg_ast[0])
+	# 	# Write the AST file
+	# 	ast_file.write(repr(cfg_ast[0]))
+	# 	# Write the CFG file
+	# 	cfg_file.write("\n".join([repr(x) for x in cfg]))
+	# else:
+	# 	for message in messages:
+	# 		print(message)
 
-	ast_file.close()
-	cfg_file.close()
+	# ast_file.close()
+	# cfg_file.close()
