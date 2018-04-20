6fa39d83d4eb47f4fbd44d33a2e449098d98e741
rohitrango <rohit.rango@gmail.com>
2018-04-20 15:10:36 +0530

    update mtest to automate everything

--- A5/reference/mtest.sh
+++ A5/reference/mtest.sh
@@ -1,5 +1,25 @@
-rm $1.s $1.s.s
-python3 ../Parser.py $1
-cp $1.s $1.s.s
-./test.sh $1
-diff $1.s $1.s.s
+# For correct
+for fi in correct/*.c;
+do
+	python3 ../Parser.py $fi
+	cp $fi.s $fi.s.s
+	cp $fi.ast $fi.s.ast
+	cp $fi.cfg $fi.s.cfg
+	cp $fi.sym $fi.s.sym
+	./test.sh $fi
+	diff $fi.s $fi.s.s
+	# diff -w $fi.ast $fi.s.ast
+	# diff -w $fi.cfg $fi.s.cfg
+	# diff -w $fi.sym $fi.s.sym
+done
+
+# For custom
+for fi in custom/*.c;
+do
+	python3 ../Parser.py $fi
+	cp $fi.s $fi.s.s
+	./test.sh $fi
+	diff $fi.s $fi.s.s
+done
+
+
